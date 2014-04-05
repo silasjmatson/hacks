6430d86f70dbf67d5369780c39d2895c60eafdb3
Silas J. Matson <silas@clearsightstudio.com>
2014-04-04 21:30:41 -0700

    Work on mylisp

--- mylisp/parsing.c
+++ mylisp/parsing.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mpc.h"
 
-/* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
-
-#include <string.h>
 
 static char buffer[2048];
 
-/* Fake readline function */
-char* readline(char* prompt) {
+char *readline(char* prompt) {
   fputs("lispy> ", stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
+
+  char* cpy = malloc(strlen(buffer) + 1);
   strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
+  cpy[strlen(cpy) - 1] = '\0';
   return cpy;
 }
 
-/* Fake add_history function */
 void add_history(char* unused) {}
 
-/* Otherwise include the editline headers */
 #else
 
 #include <readline/readline.h>
@@ -30,67 +24,48 @@ void add_history(char* unused) {}
 
 #endif
 
+// Lexer Defining & Parsing
+#include "mpc.h"
+
+
 int main(int argc, char** argv) {
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+  mpc_parser_t* Number    = mpc_new("number");
+  mpc_parser_t* Decimal   = mpc_new("decimal");
+  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Expr      = mpc_new("expr");
+  mpc_parser_t* Lispy     = mpc_new("lispy");
 
-  /* Create Some Parsers */
-  mpc_parser_t* Number   = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
-
-  /* Define them with the following Language */
   mpca_lang(MPC_LANG_DEFAULT,
-    "                                                     \
-     number   : /-?[0-9]+/ ;                             \
-     operator : '+' | '-' | '*' | '/' | '%' ;                  \
-     expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-     lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    "                                                                     \
+      number    : /-?[0-9]+/ ;                                            \
+      decimal   : // <number>+ /[.]+[0-9]+/ ;                             \
+      operator  : '+' | '-' | '*' | '/' | '%' ;                           \
+      expr      : <number> | <decimal> | '(' <operator> <expr>+ ')' ;     \
+      lispy     : /^/ <operator> <expr>+ /$/ ;                            \
     ",
-    Number, Operator, Expr, Lispy
+    Number, Decimal, Operator, Expr, Lispy
   );
 
- while (1) {
+
+  puts("Lispy Version 0.0.0.0.1");
+  puts("This is my lisp!");
+  puts("Press Ctrl-C to Exit\n");
+
+  while (1) {
     char* input = readline("lispy> ");
 
-    /* Attempt to Parse the user Input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      /* Load AST from output */
-      mpc_ast_t* a = r.output;
-      printf("Tag: %s\n", a->tag);
-      printf("Contents: %s\n", a->contents);
-      printf("Number of children: %i\n", a->children_num);
-
-      /* Get First Child */
-      mpc_ast_t* c0 = a->children[0];
-      printf("First Child Tag: %s\n", c0->tag);
-      printf("First Child Contents: %s\n", c0->contents);
-      printf("First Child Number of children: %i\n", c0->children_num);
+      /* Successfully parsed the input as 'Lispy' */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
     } else {
-      /* Otherwise Print the Error */
+      /* Errored */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-
-    add_history(input);
-
   }
 
-  /* Undefine and Delete our Parsers */
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
-
+  mpc_cleanup(5, Number, Decimal, Operator, Expr, Lispy);
   return 0;
-}
-
-int number_of_nodes(mpc_ast_t* t) {
-  if (t->children_num == 0) { return 1; }
-  if (t->children_num >= 1) {
-      int total = 1;
-      for (int i = 0; i < t->children_num; i++) {
-            total = total + number_of_nodes(t->children[i]);
-          }
-      return total;
-    }
 }
